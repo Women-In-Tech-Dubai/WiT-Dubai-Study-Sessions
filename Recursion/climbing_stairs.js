39dd1c5d96779f3159bd3dae05911f8dd4fa6ae9
amalamine <amal@>
2020-04-26 21:06:50 +0400

    added recursion solutions

---
+++ Recursion/climbing_stairs.js
@@ -0,0 +1,66 @@
+/**
+ * Recursion
+ * Sunday, April 26th
+ * https://leetcode.com/problems/climbing-stairs/
+ */
+
+/**
+ *
+ * You are climbing a stair case. It takes n steps to reach to the top.
+ *
+ * Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top
+ *
+ * Note: Given n will be a positive integer.
+ *
+ * Example 1:
+ * Input: 2
+ * Output: 2
+ * Explanation: There are two ways to climb to the top.
+ * 1. 1 step + 1 step
+ * 2. 2 steps
+ *
+ * Example 2:
+ * Input: 3
+ * Output: 3
+ * Explanation: There are three ways to climb to the top.
+ * 1. 1 step + 1 step + 1 step
+ * 2. 1 step + 2 steps
+ * 3. 2 steps + 1 step
+ *
+ */
+
+ // 1. Base Cases: currentStep == N, currentStep > N
+ // 2. Recursive sequence
+
+
+ /**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+    return climbStairsH(0, n);
+};
+var climbStairsH= function(currentStep, top) {
+    if(currentStep === top) {
+        return 1;
+    }
+    if (currentStep > top) {
+        return 0;
+    }
+    return climbStairsH(currentStep + 1, top) + climbStairsH(currentStep + 2, top);
+}
+
+console.log(climbStairs(5))
+
+var climbStairsIterative = function (N) {
+    var memo = [];
+    memo[0] = 0;
+    memo[1] = 1;
+    memo[2] = 2;
+
+    for(let i = 3; i <= N; i++) {
+        memo[i] = memo[i-1] + memo[i-2];
+    }
+
+    return memo[N];
+}
---
+++ Recursion/fibonacci.js
@@ -0,0 +1,55 @@
+/**
+ * Recursion
+ * Sunday, April 26th
+ * https://leetcode.com/problems/fibonacci-number/
+
+/**
+ *  Fibonacci Number
+ *
+ * The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1. 
+ * That is,
+ * F(0) = 0,   F(1) = 1
+ * F(N) = F(N - 1) + F(N - 2), for N > 1.
+ * Given N, calculate F(N).
+ *
+ * Input: 2
+ * Output: 1
+ * Explanation: F(2) = F(1) + F(0) = 1 + 0 = 1.
+ *
+ * Input: 3
+ * Output: 2
+ * Explanation: F(3) = F(2) + F(1) = 1 + 1 = 2.
+ *
+ * Input: 4
+ * Output: 3
+ * Explanation: F(4) = F(3) + F(2) = 2 + 1 = 3.
+ */
+
+/**
+* @param {number} :)
+* @return {number}
+*/
+
+// Big O = O(2^n)
+var fib = function (N) {
+    if (N <= 1) {
+        // O(1)
+        return N;
+    }
+    return fib(N-1) + fib(N-2);
+};
+
+// Memoization = storing values that have already been computed - Maria De Kewl :)
+// Big O with memoization: O(N)
+var fibM = function (N) {
+    let memo = {};
+    if (N <= 1) {
+        return N;
+    }
+    if (memo[N]) {
+        return memo[N];
+    }
+    let result = fib(N-1) + fib(N-2);
+    memo[N] = result;
+    return result;
+};
