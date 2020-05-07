9402cb737d3ec403abce1c1dd1e8e0513e0b2cae
Maria <mariahcdcosta@gmail.com>
2020-05-07 10:49:17 +0400

    Code clean ups for unique paths solution

--- Recursion/unique_paths.js
+++ Recursion/unique_paths.js
@@ -6,9 +6,9 @@
 
 /**
  *
- * A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+ * A robot is located at the top-left corner of a m x n grid.
  *
- * The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+ * The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid
  *
  * How many possible unique paths are there?
  *
@@ -29,17 +29,20 @@
  * - 1 <= m, n <= 100
  * - It's guaranteed that the answer will be less than or equal to 2 * 10 ^ 9.
  */
+
+/**
+* Initution:-
+* Base cases: 
+* a. boundary check
+* b. when bottom-right cell reached
+* Recursive relation: determine sum of paths from the right cell to the target + paths from the bottom cell to the target
+*/
+
 /**
  * @param {number} cols
  * @param {number} rows
  * @return {number}
  */
-
-// Base cases: a. boundary check b. when bottom-right cell reached
-// Recursive relation: process right cell, process bottom cell
-
-// let paths = 0;
-
 var uniquePaths = function (cols, rows) {
     let memo = [[]];
     return helper(cols - 1, rows - 1, 0, 0, memo);
@@ -52,10 +55,10 @@ var uniquePaths = function (cols, rows) {
  * 1 - - x
  * target = grid[1][2]
  *
- *                 (0,0)
- *       (0,1)               (1, 0)
- * (0, 2)      (1,1)      (1,2)    (2,0)
- *         (2, 1) (1,2)
+ *                              (0,0)
+ *              (0, 1)                              (1, 0)
+ *      (0, 2)          (1, 1)              (1, 1)           (2, 1)   
+ * (0, 3)   (1, 2)   (1, 2)  (2, 1)     (1, 2)  (2, 1) 
  */
 const helper = (targetCol, targetRow, col, row, memo) => {
 
