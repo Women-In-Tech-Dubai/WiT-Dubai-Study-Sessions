142ecc39d7b31be21e451714e861eb53d08d8f58
amalamine <amal@>
2020-08-08 19:15:47 +0400

    added climbing stairs

---
+++ Recursion/climbing_stairs/breakout_room_4.js
@@ -0,0 +1,43 @@
+/**
+ * Recursion
+ * Sunday, August 9th
+ * https://leetcode.com/problems/climbing-stairs/
+ */
+
+/**
+ *
+ * You are climbing a stair case. It takes n steps to reach to the top.
+ *
+ * Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top
+ *
+ * Note: Given n will be a positive integer.
+ *
+ * Input: 2
+ * Output: 2
+ * Explanation: There are two ways to climb to the top.
+ * 1. 1 step + 1 step
+ * 2. 2 steps
+ *
+ * Input: 3
+ * Output: 3
+ * Explanation: There are three ways to climb to the top.
+ * 1. 1 step + 1 step + 1 step
+ * 2. 1 step + 2 steps
+ * 3. 2 steps + 1 step
+ *
+ * Input: 4
+ * Output: 5
+ * Explanation: There are five ways to climb to the top.
+ * 1. 1 step + 1 step + 1 step + 1 step
+ * 2. 1 step + 2 steps + 1 step
+ * 3. 2 steps + 1 step + 1 step
+ * 4. 1 step + 1 step + 2 steps
+ * 5. 2 steps + 2 steps
+ */
+
+ /**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+};
