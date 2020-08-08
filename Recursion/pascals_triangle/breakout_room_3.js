0ecba69d94b5a5a887b1fe32c2c93849dd9e1fd3
amalamine <amal@>
2020-08-08 19:13:37 +0400

    added pascals triangle

---
+++ Recursion/pascals_triangle/breakout_room_3.js
@@ -0,0 +1,31 @@
+/**
+ * Recursion
+ * Sunday, August 9th
+ * https://leetcode.com/problems/pascals-triangle/
+ */
+
+/**
+ *
+ * Given a non-negative integer numRows, generate the first numRows of Pascal's triangle.
+ *
+ * In Pascal's triangle, each number is the sum of the two numbers directly above it.
+ *
+ *               1
+ *            1     1
+ *         1     2    1
+ *      1     3     3   1
+ *    1    4     6    4    1
+ * 1    5    10    10    5    1
+ *
+ *
+ * Example:
+ * Input: 5
+ * Output:
+ * [
+ *  [1],
+ *  [1,1],
+ *  [1,2,1],
+ *  [1,3,3,1],
+ *  [1,4,6,4,1]
+ * ]
+ */
