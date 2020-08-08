1ab1e73bb10e8c9331636102460dddda373283a9
amalamine <amal@>
2020-08-08 19:00:24 +0400

    added solution

--- Binary trees/sameTree.js
+++ Binary trees/sameTree.js
@@ -1,4 +1,4 @@
-/**
+/*
  * https://leetcode.com/problems/same-tree/
  * 
  * Given two binary trees, write a function to check if they are the same or not.
@@ -36,17 +36,29 @@
  */
 /**
  * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
- * }
  */
-/**
- * @param {TreeNode} p
- * @param {TreeNode} q
- * @return {boolean}
- */
-var isSameTree = function(p, q) {
-    
-};
+function TreeNode(val, left, right) {
+       this.val = (val===undefined ? 0 : val)
+       this.left = (left===undefined ? null : left)
+       this.right = (right===undefined ? null : right)
+  }
+ 
+ /**
+  * @param {TreeNode} p
+  * @param {TreeNode} q
+  * @return {boolean}
+  */
+ /**
+  * Base cases: 
+  * 1. leftTreeNode or rightTreeNode === null
+  * 2. leftTreeNode.val !== rightTreeNode.val
+  */
+
+ var isSameTree = function(p, q) {
+     if(p === null && q === null) return true;
+     if(p === null || q === null) return false;
+
+     if(p.val !== q.val) return false;
+     return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+ };
+
