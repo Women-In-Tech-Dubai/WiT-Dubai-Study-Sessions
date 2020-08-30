1477ae8fa9791c59b5517fad6c0e209c934dbe08
Amal Amine and Jorge Vilaseco <amal@>
2020-08-30 18:27:29 +0400

    added fib breakout files

---
+++ Recursion/fibonacci/breakout_room_1.js
@@ -0,0 +1,34 @@
+/**
+ * Recursion, DP
+ * https://leetcode.com/problems/fibonacci-number/
+ */
+
+/**
+ *  Fibonacci Number
+ *
+ * The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1. 
+ * That is,
+ * F(0) = 0,   F(1) = 1
+ * F(N) = F(N - 1) + F(N - 2), for N > 1.
+ * Given N, calculate F(N).
+ *
+ * Input: 2
+ * Output: 1
+ * Explanation: F(2) = F(1) + F(0) = 1 + 0 = 1.
+ *
+ * Input: 3
+ * Output: 2
+ * Explanation: F(3) = F(2) + F(1) = 1 + 1 = 2.
+ *
+ * Input: 4
+ * Output: 3
+ * Explanation: F(4) = F(3) + F(2) = 2 + 1 = 3.
+ */
+
+/**
+* @param {number} :)
+* @return {number}
+*/
+
+var fib = function (N) {
+};
