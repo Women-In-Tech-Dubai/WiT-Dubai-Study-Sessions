50aac2dd4bec618aadc726ccc58d1ba731718a85
Amal Amine <amal@>
2021-03-07 20:28:06 +0400

    added solutions

--- Arrays/boats_to_save_people/1.js
+++ Arrays/boats_to_save_people/1.js
@@ -29,3 +29,45 @@ Constraints:
 1 <= people.length <= 5 * 104
 1 <= people[i] <= limit <= 3 * 104
 */
+
+// [3,5,1,3,4] limit = 5
+// [1,3,3,4,5]
+// [5, 4, 3, 3, 1]
+// 1. Sort people array desc
+// 2. for each index, another pointer (j) that iterates through the rest of the array
+// 3. if (people[i] == limit) count++
+// 3a. if (people[i] < limit) => totalWeight < limit, count++, remove people[j] OR totalWeight > limit, j++ OTHERWISE count++
+
+
+// 1. Sort people array
+// 2. Two pointers: smallest and biggest
+// 3. smallest < people.length && biggest >= 0 && biggest <= smallest
+// 4. if (people[biggest] == limit) => count++, biggest--;
+// 4a. if (people[biggest] < limit) => totalWeight
+
+
+/**
+ * @param {number[]} people
+ * @param {number} limit
+ * @return {number}
+ */
+ var numRescueBoats = function(people, limit) {
+    let sortedPeople = people.sort((a, b) => b - a);
+    let numberOfBoats = 0;
+
+    for(let i = 0; i < sortedPeople.length; i++) {
+        if (sortedPeople[i] == limit) {
+            numberOfBoats++;
+        } else {
+            for(let j = i+1; j < sortedPeople.length; j++) {
+                let totalWeight = sortedPeople[i] + sortedPeople[j];
+                if (totalWeight <= limit) {
+                    sortedPeople.splice(j, 1);
+                    break;
+                }
+            }
+            numberOfBoats++;
+        }
+    }
+    return numberOfBoats;
+};
--- Strings/minimum_swaps/1.js
+++ Strings/minimum_swaps/1.js
@@ -7,19 +7,18 @@ You are given two strings s1 and s2 of equal length consisting of letters "x" an
 
 Return the minimum number of swaps required to make s1 and s2 equal, or return -1 if it is impossible to do so.
 
- 
 
 Example 1:
 
 Input: s1 = "xx", s2 = "yy"
 Output: 1
-Explanation: 
+Explanation:
 Swap s1[0] and s2[1], s1 = "yx", s2 = "yx".
-Example 2: 
+Example 2:
 
 Input: s1 = "xy", s2 = "yx"
 Output: 2
-Explanation: 
+Explanation:
 Swap s1[0] and s2[0], s1 = "yy", s2 = "xx".
 Swap s1[0] and s2[1], s1 = "xy", s2 = "xy".
 Note that you can't swap s1[0] and s1[1] to make s1 equal to "yx", cause we can only swap chars in different strings.
@@ -31,7 +30,7 @@ Example 4:
 
 Input: s1 = "xxyyxyxyxx", s2 = "xyyxyxxxyx"
 Output: 4
- 
+
 
 Constraints:
 
@@ -39,3 +38,58 @@ Constraints:
 s1, s2 only contain 'x' or 'y'.
 
 */
+
+/**
+ * Rules:
+1. Each pair of x_y and x_y => 1 swap
+2. Each pair of y_x and y_x => 1 swap
+3. If left with one pair of any one => return -1
+4. If left with one pair of both => swap + 2
+*/
+
+// 1. Count x_y and y_x
+// 2. swap += Math.floor(x_y / 2);
+// 3. remainder += (x_y/2) - Math.floor(x_y / 2);
+// 4. swap += Math.floor(y_x / 2);
+// 5. remainder += (y_x/2) - Math.floor(y_x / 2);
+// 6. (if remainder is fraction) => return -1 else swaps += remainder * 2
+
+/**
+ * @param {string} s1
+ * @param {string} s2
+ * @return {number}
+ */
+ var minimumSwap = function(s1, s2) {
+    if (s1.length !== s2.length) {
+        return -1;
+    }
+
+    let xy_count = 0;
+    let yx_count = 0;
+
+    for (let i=0; i<s1.length; i++) {
+        if (s1[i] == 'x' && s2[i] == 'y') {
+            xy_count++;
+        }
+        else if (s1[i] == 'y' && s2[i] == 'x') {
+            yx_count++;
+        }
+    }
+
+    let swaps = 0;
+    let remainder = 0;
+    swaps += Math.floor(xy_count/2);
+    remainder += (xy_count/2) - Math.floor(xy_count / 2);
+    swaps += Math.floor(yx_count/2);
+    remainder += (yx_count/2) - Math.floor(yx_count / 2);
+
+    if (remainder % 1 > 0) {
+        return -1;
+    }
+
+    if (remainder % 1 == 0) {
+        swaps += (remainder * 2)
+    }
+
+    return swaps;
+};
