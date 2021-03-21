09c8eae25ab5c8ddd3999e1f51f6bf1a543dd741
Amal Amine <amal@>
2021-03-21 16:54:03 +0400

    added questions

---
+++ Arrays/pack_numbers/1.js
@@ -0,0 +1,24 @@
+/*
+
+Pack numbers
+
+Given an array of integers, write a method that will pack neighbouring numbers that are the same into the string X:N, where X is the integer value
+
+and N is the sequence length.
+
+Example:
+
+packNumbers ((5, 5, 5, 7, 1, 3, 4, 7])
+
+should return
+
+[5:3', 7.2', 3, 4, 7]
+
+
+packNumbers ((5, 5, 5, 7, 1, 3, 4, 7, 5])
+
+should return
+
+[5:3', 7.2', 3, 4, 7, 5]
+
+*/
