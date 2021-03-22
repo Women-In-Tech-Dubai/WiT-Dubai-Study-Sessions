7222a2a4211cf6d688b33acc972590f44d0d332c
Amal Amine <amal@>
2021-03-22 10:00:29 +0400

    added solutions

--- Arrays/unlucky_floors/1.js
+++ Arrays/unlucky_floors/1.js
@@ -1,16 +1,44 @@
 /*
 Unlucky floors
 
-You check in at a hotel - one of the tallest buildings in the world. When taking the elevator, you realize that particular hotel has a superstition: There are no floors whose number has a '4' or '13' as part of it.
+You check in at a hotel - one of the tallest buildings, burj khalifa :-) .
+When taking the elevator, you realize that particular hotel has a superstition: There are no floors whose number has a '4' or '13' as part of it.
 
-That means that hotel has no 4th floor, nor a 13th, nor a 134th, nor a 113rd - but they do have the 103rd. Therefore, first floors are numbered as 1, 2, 3, 5, 6, 7, 8, 9, 10, 11, 12, 15, 16, and so on...
+That means that hotel has no 4th floor, nor a 13th, nor a 134th, nor a 113th - but they do have the 103rd.
+Therefore, first floors are numbered as 1, 2, 3, 5, 6, 7, 8, 9, 10, 11, 12, 15, 16, and so on...
 
-The hotel wants to build a second tower but they don't want to go through the hassle of numbering floors by hand. They want to have an algorithm that given a "real" floor number, returns the "lucky" version and removes any floors that contain '4' or '13' as part of their numbers.
+The hotel wants to build a second tower but they don't want to go through the hassle of numbering floors by hand.
+They want to have an algorithm that given a "real" floor number, returns the "lucky" version and removes any floors that contain '4' or '13' as part of their numbers.
 
 Example
+
 
 Given that the real floor number 12, its "lucky" version will be 15:
 
 Real:  1 2 3 4 5 6 7 8 9 10 11 12
 Lucky: 1 2 3 5 6 7 8 9 10 11 12 15
+
+
 */
+
+function isUnlucky(n) {
+    const parsedNumber = `${n}`;
+    return parsedNumber.indexOf('4') > -1 || parsedNumber.indexOf('13') > -1;
+}
+
+function processData(input) {
+    let luckyFloors = [];
+
+    let i = 1;
+    while (luckyFloors.length != input) {
+        if(!isUnlucky(i)) {
+            luckyFloors.push(i)
+        }
+        i++;
+    }
+
+    console.log(luckyFloors);
+    return luckyFloors[input-1];
+} 
+
+console.log(processData(444));
--- Strings/robot_rodeo/1.js
+++ Strings/robot_rodeo/1.js
@@ -13,27 +13,97 @@ The robot performs the instructions given in order, and repeats them forever.
 Return true if and only if there exists a circle in the plane such that the robot never leaves the circle.
 
 
-
 Example 1:
 
 Input: instructions = "GGLLGG"
 Output: true
 Explanation: The robot moves from (0,0) to (0,2), turns 180 degrees, and then returns to (0,0).
 When repeating these instructions, the robot remains in the circle of radius 2 centered at the origin.
-Example 2:
 
+Example 2:
 Input: instructions = "GG"
 Output: false
 Explanation: The robot moves north indefinitely.
-Example 3:
 
+Example 3:
 Input: instructions = "GL"
 Output: true
 Explanation: The robot moves from (0, 0) -> (0, 1) -> (-1, 1) -> (-1, 0) -> (0, 0) -> ...
-
 
 Constraints:
 
 1 <= instructions.length <= 100
 instructions[i] is 'G', 'L' or, 'R'.
+
+
 */
+
+const getNewDirection = (current, move) => {
+    if (move == "L") {
+        switch(current) {
+            case 'N':
+                return 'W';
+            case 'W': 
+                return 'S';
+            case 'S':
+                return 'E';
+            default: 
+                return 'N';
+        }
+    } else {
+        switch(current) {
+            case 'N':
+                return 'E';
+            case 'W': 
+                return 'N';
+            case 'S':
+                return 'W';
+            default: 
+                return 'S';
+        }
+    }
+}
+
+getNewPosition = function(direction, { x, y }) {
+    switch(direction) {
+        case 'N':
+            y++;
+            break;
+        case 'E':
+            x++;
+            break;
+        case 'S':
+            y--;
+            break;
+        case 'W':
+            x--;
+            break;
+    }
+
+    return { x, y };
+}
+
+/**
+ * @param {string} instructions
+ * @return {boolean}
+ */
+ var isRobotBounded = function(instructions) {
+    let x = 0;
+    let y = 0;
+    let currentDirection = 'N';
+
+    let moves = instructions.split('');
+
+    moves.forEach((move) => {
+        if (move == "L") {
+            currentDirection = getNewDirection(currentDirection, "L");
+        } else if (move == "R") {
+            currentDirection = getNewDirection(currentDirection, "R");
+        } else {
+            ({x, y} = getNewPosition(currentDirection, {x, y}));
+        }
+    });
+    return (x == 0 && y == 0);
+};
+
+console.log(isRobotBounded("GL"));
