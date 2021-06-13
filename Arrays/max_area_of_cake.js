6ccd3853fa9958dae164bb36a33baa8763b2c99b
Maria D'Costa <maria@expensify.com>
2021-06-13 17:30:35 +0400

    Clean up solution for max area of cake

--- Arrays/max_area_of_cake.js
+++ Arrays/max_area_of_cake.js
@@ -16,34 +16,20 @@
     const mod = 1000000007;
 
     horizontalCuts.sort((a, b) => a - b);
-    for(let i = 0; i < horizontalCuts.length; i++) {
-        let prevEdge = horizontalCuts[i - 1] ? horizontalCuts[i - 1] : 0;
-        maxVertical = Math.max(maxVertical, horizontalCuts[i] - prevEdge);
-        console.log(`maxVertical`, maxVertical);
-
-        if (i == horizontalCuts.length - 1) {
-            maxVertical = Math.max(maxVertical, height - horizontalCuts[i]);
-        }
-        console.log(`maxVertical`, maxVertical);
-
+    maxVertical = height - horizontalCuts[horizontalCuts.length - 1];
+    for(let i = 1; i < horizontalCuts.length; i++) {
+        maxVertical = Math.max(maxVertical, horizontalCuts[i] - horizontalCuts[i-1]);
     }
-
-    console.log(`maxVerticalFinal`, maxVertical);
     
     verticalCuts.sort((a, b) => a - b);
-    for(let i = 0; i < verticalCuts.length; i++) {
-        let prevEdge = verticalCuts[i - 1] ? verticalCuts[i - 1] : 0;
-        maxHorizontal = Math.max(maxHorizontal, verticalCuts[i] - prevEdge);
-        if (i == verticalCuts.length - 1) {
-            maxHorizontal = Math.max(maxHorizontal, width - verticalCuts[i]);
-        }
+    maxHorizontal = width - verticalCuts[verticalCuts.length - 1];
+    for(let i = 1; i < verticalCuts.length; i++) {
+        maxHorizontal = Math.max(maxHorizontal, verticalCuts[i] - verticalCuts[i - 1]);
     }
 
-    console.log(`maxHorizontal`, maxHorizontal);
-
-    return maxHorizontal * maxVertical % 1000000007;
+    return (maxHorizontal * maxVertical) % mod;
 };
 
-// console.log(maxArea(5, 4, [3,1], [1]));
-// console.log(maxArea(5, 4, [1,2,4], [1,3]));
+console.log(maxArea(5, 4, [3,1], [1]));
+console.log(maxArea(5, 4, [1,2,4], [1,3]));
 console.log(maxArea(1000000000, 1000000000,[2], [2]));
