f1908f64c2492947c82edefee0f286527fc8d991
amalamine <amalamine00@gmail.com>
2021-09-07 10:25:14 +0400

    add question

---
+++ Strings/excel_sheet_columns.js
@@ -0,0 +1,65 @@
+/**
+ *
+ *  https://leetcode.com/problems/excel-sheet-column-title/
+    Given an integer columnNumber, return its corresponding column title as it appears in an Excel sheet.
+
+    For example:
+
+    A -> 1
+    B -> 2
+    C -> 3
+    ...
+    Z -> 26
+    AA -> 27
+    AB -> 28 
+    ...
+    
+
+    Example 1:
+
+    Input: columnNumber = 1
+    Output: "A"
+
+    Example 2:
+
+    Input: columnNumber = 28
+    Output: "AB"
+
+    Example 3:
+
+    Input: columnNumber = 701
+    Output: "ZY"
+
+    Example 4:
+
+    Input: columnNumber = 2147483647
+    Output: "FXSHRXW"
+    
+
+    Constraints:
+
+    1 <= columnNumber <= 2^31 - 1
+
+ */
+
+/**
+ * @param {number} columnNumber
+ * @return {string}
+ */
+ var convertToTitle = function(columnNumber) {
+    let title = '';
+    let remainder = 0;
+
+    while(columnNumber > 0) {
+      remainder = columnNumber % 26;
+      // Map to a letter, concatenating columnTitle
+      remainder = remainder === 0 ? 26 : remainder;
+      title += String.fromCharCode(remainder + 64);
+      columnNumber = (columnNumber - remainder) / 26;
+
+      console.log("columnNumber: ", columnNumber);
+    }
+    return title.split('').reverse().join('');
+};
+
+console.log(convertToTitle(52));
