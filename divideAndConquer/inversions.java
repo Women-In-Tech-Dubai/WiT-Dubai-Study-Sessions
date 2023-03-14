f62e6513fbd74c2a9b0f0369098c805d48787c38
djtanner <zshariat08@gmail.com>
2023-03-14 22:09:21 +0400

    Added inversions java with solution

---
+++ divideAndConquer/inversions.java
@@ -0,0 +1,63 @@
+public class Inversions {
+    // Return the number of inversions in the permutation a[].
+    public static long count(int[] a) {
+        //to do
+        return 1;
+    }
+
+    // Return a permutation of length n with exactly k inversions.
+    public static int[] generate(int n, long k) {
+        int[] perm0 = new int[n];
+        int[] perm = new int[n];
+
+        // If n or k are 0
+        for (int i = 0; i < n; i++) {
+            perm0[i] = i;
+        }
+
+        if (n == 0 || k == 0) return perm0;
+
+
+        for (int i = 0; i < n; i++) {
+
+            if (k >= n - i) {
+                perm[i] = n - i - 1;
+
+            }
+
+            else if (k > 0 && k < n) {
+                perm[n - 1 - (int) k] = n - i - 1;
+
+            }
+            
+            k = k - n + i + 1;
+        }
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+
+            if (perm[i] == 0) {
+                perm[i] = count;
+                count++;
+
+            }
+
+        }
+
+
+        return perm;
+    }
+
+
+    // Takes an integer n and a long k as command-line arguments,
+    // and prints a permutation of length n with exactly k inversions.
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        long k = Long.parseLong(args[1]);
+
+        int[] perm = generate(n, k);
+
+        for (int i = 0; i < perm.length; i++) {
+            System.out.print(perm[i] + " ");
+        }
+    }
+}
